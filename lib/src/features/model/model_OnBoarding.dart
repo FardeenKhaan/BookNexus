15410da7f7fe55c208d26c788e39b364064361dc
abdul Razzaq <ar8937562@gmail.com>
2024-10-07 21:16:04 +0500

    created model_onboarding class and its constructor

---
+++ lib/src/features/model/model_OnBoarding.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ModelOnboarding {
+  final String image;
+  final String title;
+  final String subtitle;
+  final String counterText;
+  final Color bgcolor;
+
+  ModelOnboarding({
+    required this.image,
+    required this.title,
+    required this.subtitle,
+    required this.counterText,
+    required this.bgcolor,
+  });
+}
